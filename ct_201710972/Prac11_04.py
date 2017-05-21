9b0bb7a8c62bdd80828d5815538ccea7b9c1a803
hhbb22 <bi1724@gmail.com>
2017-05-21 22:14:25 +0900

    Add files via upload

---
+++ ct_201710972/Prac11_04.py
@@ -0,0 +1,11 @@
+﻿from PIL import Image
+s= Image.open("C:\\Users\\400T6B\\Desktop\\italy.jpg")
+s.show()
+
+%matplotlib inline
+from matplotlib.pyplot import imshow
+imshow(s)
+
+from PIL import Image
+s= Image.open ("C:\\Users\\400T6B\Desktop\\italy.jpg")
+s.save("C:\\Users\\400T6B\\Desktop\\italy.png")
