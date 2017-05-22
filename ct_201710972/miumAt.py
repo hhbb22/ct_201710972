f1553750b597146238b736544bca6af427ae33cb
hhbb22 <bi1724@gmail.com>
2017-05-22 17:06:39 +0900

    Add files via upload

---
+++ ct_201710972/miumAt.py
@@ -0,0 +1,25 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+
+def giyukAt(x,y,size):  
+    t1.goto(x,y)
+    t1.lt(90)  
+    t1.fd(size)
+    t1.lt(90)
+    t1.fd(size)
+
+def niunAt(size):    
+    t1.lt(90)
+    t1.fd(size)
+    t1.lt(90)
+    t1.fd(size)
+
+def miumAt(x,y,size):
+    t1.goto(x,y)
+    giyukAt(x,y,size)
+    niunAt(size)
+
+
+miumAt(-100,100,200)
