52e8663c330756d1815c6184c380aa2e7285dc08
hhbb22 <bi1724@gmail.com>
2017-05-29 16:43:44 +0900

    Add files via upload

---
+++ ct_201710972/201710972_10-5.py
@@ -0,0 +1,118 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+    
+win = turtle.Screen()
+width = win.window_width()
+w3 = (width - 40) / 3
+
+x1 = 0.0 - w3
+x2 = 0.
+x3 = 0. + w3
+
+
+t1.penup()
+t1.goto(x1, 0)
+t1.pendown()
+t1.pos()
+t1.write(t1.pos())
+
+t1.clear()
+
+
+def drawTriangleAt():
+    t1.forward(100)
+    t1.write(t1.pos())
+    t1.left(120)
+    t1.forward(100)
+    t1.write(t1.pos())
+    t1.left(120)
+    t1.forward(100)
+    t1.write(t1.pos())
+    t1.left(120)
+
+        
+
+def drawPentagon():
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+
+
+def drawStarAt():
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.rt(144)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.rt(144)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.rt(144)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.rt(144)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.rt(144)
+
+
+
+
+drawTriangleAt()
+
+t1.reset()
+
+t1.penup()
+t1.goto(x1, 0)
+t1.pendown()
+t1.pos()
+t1.write(t1.pos())
+
+t1.clear()
+
+
+drawPentagon()
+
+t1.penup()
+t1.goto(x1, 0)
+t1.pendown()
+t1.pos()
+t1.write(t1.pos())
+
+t1.clear()
+
+
+t1.reset()
+
+t1.penup()
+t1.goto(x1, 0)
+t1.pendown()
+t1.pos()
+t1.write(t1.pos())
+
+t1.clear()
+
+
+drawStarAt()
+
+
+
+
+    
+
+
+    
