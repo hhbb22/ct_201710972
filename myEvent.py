64082b50e4007c9b27afd7937ef63b695131b832
hhbb22 <bi1724@gmail.com>
2017-06-05 17:25:44 +0900

    Add files via upload

---
+++ myEvent.py
@@ -0,0 +1,15 @@
+﻿import turtle
+win=turtle.Screen()
+t1=turtle.Turtle()
+
+def keyup():
+ pt=t1.pos()
+ print("up",pt)
+ t1.write(pt)
+ t1.fd(45)
+
+win.onkey(keyup,"Up")
+win.listen()
+turtle.mainloop()
+
+
